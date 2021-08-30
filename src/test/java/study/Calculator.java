319146cc63dda4f4550d82fd5b5c901599cd7aa9
jrlee <jrlee@cyglobal.net>
2021-08-30 12:42:27 +0900

    추상화 수준 조정

--- src/test/java/study/Calculator.java
+++ src/test/java/study/Calculator.java
@@ -2,9 +2,13 @@ package study;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.junit.platform.commons.util.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -50,37 +54,41 @@ public class Calculator {
     public int freeCal(String input) {
         String[] split = input.split(" ");
 
+        List<String> operatorList = Arrays.stream(split)
+                                          .filter(o -> isOperator(o))
+                                          .collect(Collectors.toList());
+        List<String> numberList = Arrays.stream(split)
+                                          .filter(o -> !isOperator(o))
+                                          .collect(Collectors.toList());
+
         //when
-
         Stack<String> stack = new Stack<>();
-        for (String s : split) {
-
-            if(isOperator(s)){
-                stack.push(s);
-                continue;
-            }
-
+        for (String s : numberList) {
             if(stack.isEmpty()){
                 stack.push(s);
                 continue;
             }
 
-            int tempResult = 0;
-            String operator = stack.pop();
-            String first = stack.pop();
-            switch (operator){
-                case "+" : tempResult = add(first, s); break;
-                case "-" : tempResult = minus(first, s); break;
-                case "*" : tempResult = mul(first, s); break;
-                case "/" : tempResult = div(first, s); break;
-            }
-
-            stack.push(String.valueOf(tempResult));
+            String beforeNumber = stack.pop();
+            String operator = operatorList.get(0);
+            operatorList.remove(0);
+            int result = calByAutoSelectedOperator(beforeNumber, operator, s);
+            stack.push(String.valueOf(result));
         }
 
         int result = Integer.parseInt(stack.pop()) ;
         return result;
+    }
 
+    private int calByAutoSelectedOperator(String left, String operator, String right) {
+        int result = 0;
+        switch (operator){
+            case "+" : result = add(left, right); break;
+            case "-" : result = minus(left, right); break;
+            case "*" : result = mul(left, right); break;
+            case "/" : result = div(left, right); break;
+        }
+        return result;
     }
 
     @Test
