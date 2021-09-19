d263c2e13ae1cae5b5ae4fdcf2634d30f54e90af
jrlee <jrlee@cyglobal.net>
2021-09-19 21:43:17 +0900

    인풋처리 어떻게 표현하지...

--- src/test/java/study/NumberBaseballGame/NumberBaseballGame.java
+++ src/test/java/study/NumberBaseballGame/NumberBaseballGame.java
@@ -6,18 +6,37 @@ public class NumberBaseballGame {
 
 
     public void simulate() throws Exception {
-        Enemy enemy = new Enemy();
-        enemy.registAnswer("123");
 
-        System.out.println("숫자를 입력해 주세요");
-//        Scanner scanner = new Scanner(System.in);
-//        String input = scanner.nextLine();
-        AnswerResult answerResult = enemy.checkAnswer("124");
-        answerResult.PrintResult();
+        boolean simulating = true;
+        do {
+            Enemy enemy = new Enemy();
+            enemy.registAnswer("123");
 
-        if (answerResult.isAllStrike()) {
-            System.out.println("게임 종료");
-        }
+            playQuestionAndAnswer(enemy);
+
+            simulating = askReplayGame();
+        } while (simulating);
+    }
+
+    private boolean askReplayGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+
+        return false;
+    }
+
+    private void playQuestionAndAnswer(Enemy enemy) throws Exception {
+        boolean playing = true;
+        do {
+            System.out.println("숫자를 입력해 주세요");
+            String input = "124";
+
+            AnswerResult answerResult = enemy.checkAnswer(input);
+            answerResult.PrintResult();
+
+            playing = !answerResult.isAllStrike();
+        } while (playing);
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     @Test
